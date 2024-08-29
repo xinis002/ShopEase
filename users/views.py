d572300e56a58c066b8a96c961f7bf9bc07652a9
Simon Inikhov <semikinihov@gmail.com>
2024-08-29 17:56:05 +0300

    add . changes

---
+++ users/views.py
@@ -0,0 +1,41 @@
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404, redirect
+import secrets
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+
+from users.forms import UserRegisterForm
+from users.models import User
+
+from config.settings import EMAIL_HOST_USER
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(
+            subject="Подтверждение регистрации",
+            message=f"Для подтверждения регистрации перейдите по ссылке: {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
+
+
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse("users:login"))
